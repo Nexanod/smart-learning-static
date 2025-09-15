0ca27fd358a8ed447d7c0ee1b74ee4548e02ee1a
Hassan Raza <hassanraza01.dev@gmail.com>
2025-09-15 13:57:29 +0500

    First Try

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,18 +2,7 @@ import type { Metadata } from 'next';
 import ErrorReporter from '@/components/ErrorReporter';
 import Script from 'next/script';
 
-import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -27,9 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className='antialiased'>
         <ErrorReporter />
         <Script
           src='https://slelguoygbfzlpylpxfs.supabase.co/storage/v1/object/public/scripts//route-messenger.js'
